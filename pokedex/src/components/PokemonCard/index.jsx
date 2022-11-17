5be34e05fd6081543703947ce6fd364d2da1a7d8
yatocham <ederjuninho2003@gmail.com>
2022-11-17 00:30:47 -0300

    Images of pokemons, background blue, and logo pokemon

--- pokedex/src/components/PokemonCard/index.jsx
+++ pokedex/src/components/PokemonCard/index.jsx
@@ -5,15 +5,15 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
-export default function PokemonCard({ name }) {
+export default function PokemonCard({ name, image }) {
     return (
         <Card sx={{ maxWidth: 345 }}>
             <CardActionArea>
                 <CardMedia
                     component="img"
-                    height="140"
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    alt="green iguana"
+                    // height="200"
+                    image={image}
+                    alt={name}
                 />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
